9de1276d73224fbe22513788974cc805cb7e4b8f
Christian Rohrer <hydrat@gmail.com>
2011-07-12 22:20:37 +0200

    remove old stuff

--- db/migrate/20110618153939_add_producer_flag_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddProducerFlagToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :producer, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :users, :producer
-  end
-end
